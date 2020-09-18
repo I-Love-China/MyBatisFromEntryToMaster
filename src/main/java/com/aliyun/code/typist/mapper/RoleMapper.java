8a9e75195d00369970713bd3b6665698dc77d9a6
Typist <z482728291@gmail.com>
2020-09-18 21:58:42 +0800

    @Select 指定 <resultMap>: mapper.xml 中的

--- src/main/java/com/aliyun/code/typist/mapper/RoleMapper.java
+++ src/main/java/com/aliyun/code/typist/mapper/RoleMapper.java
@@ -1,9 +1,16 @@
 package com.aliyun.code.typist.mapper;
 
 import com.aliyun.code.typist.model.SysRole;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 public interface RoleMapper {
     @Select("select * from sys_role where id = #{id}")
     SysRole selectById(Long id);
+
+    @Select("select * from sys_role")
+    @ResultMap("roleMap")
+    List<SysRole> selectAll();
 }
--- src/test/java/com/aliyun/code/typist/SysRoleMapperTest.java
+++ src/test/java/com/aliyun/code/typist/SysRoleMapperTest.java
@@ -3,7 +3,10 @@ package com.aliyun.code.typist;
 import com.aliyun.code.typist.mapper.RoleMapper;
 import com.aliyun.code.typist.model.SysRole;
 import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 public class SysRoleMapperTest extends BaseMapperTest {
     @Test
@@ -11,6 +14,17 @@ public class SysRoleMapperTest extends BaseMapperTest {
         try (SqlSession sqlSession = getSqlSession()) {
             SysRole sysRole = sqlSession.getMapper(RoleMapper.class).selectById(1L);
             System.out.println(sysRole);
+        }
+    }
+
+    @Test
+    public void testSelectAll() {
+        try(SqlSession sqlSession = getSqlSession()) {
+            RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+            List<SysRole> roles = roleMapper.selectAll();
+
+            Assert.assertNotNull(roles);
+            Assert.assertTrue(!roles.isEmpty());
         }
     }
 }
