8695d36c3c358c28a3f245b63e3895c3f8834537
Typist <z482728291@gmail.com>
2020-09-18 15:28:11 +0800

    使用 <resultMap>

--- src/test/java/com/aliyun/code/typist/SysPrivilegeMapperTest.java
+++ src/test/java/com/aliyun/code/typist/SysPrivilegeMapperTest.java
@@ -4,12 +4,24 @@ import com.aliyun.code.typist.model.SysPrivilege;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.List;
+
 public class SysPrivilegeMapperTest extends BaseMapperTest {
     @Test
     public void testSelectById() {
         try (SqlSession sqlSession = getSqlSession()) {
             SysPrivilege sysPrivilege = sqlSession.selectOne("com.aliyun.code.typist.mapper.PrivilegeMapper.selectById", 1L);
             System.out.println(sysPrivilege);
+        }
+    }
+
+    @Test
+    public void testSelectAll() {
+        try (SqlSession sqlSession = getSqlSession()) {
+            List<SysPrivilege> privilegeList = sqlSession.selectList("com.aliyun.code.typist.mapper.PrivilegeMapper.selectAll");
+            for (SysPrivilege privilege : privilegeList) {
+                System.out.println(privilege);
+            }
         }
     }
 }
