74d9b7926b036fc888f7366f0e4d8bf4cb3f28ef
Typist <z482728291@gmail.com>
2020-09-19 14:50:36 +0800

    二级缓存测试 二级缓存存在于 SqlSessionFactory 的生命周期内; 当调用 close 方法关闭 SqlSession 时，SqlSession 才会保存查询数据到二级缓存中;

--- src/test/java/com/aliyun/code/typist/CacheTest.java
+++ src/test/java/com/aliyun/code/typist/CacheTest.java
@@ -1,6 +1,8 @@
 package com.aliyun.code.typist;
 
+import com.aliyun.code.typist.mapper.RoleMapper;
 import com.aliyun.code.typist.mapper.UserMapper;
+import com.aliyun.code.typist.model.SysRole;
 import com.aliyun.code.typist.model.SysUser;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Assert;
@@ -46,6 +48,37 @@ public class CacheTest extends BaseMapperTest {
             Assert.assertFalse(user2 == user3);
 
             sqlSession.rollback();
+        }
+    }
+
+    @Test
+    public void testL2Cache() {
+        SysRole role = null;
+        try (SqlSession sqlSession = getSqlSession()) {
+            RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+
+            SysRole role1 = roleMapper.selectById(1L);
+            role1.setRoleName("测试 L2Cache");
+
+            SysRole role2 = roleMapper.selectById(1L);
+            // 此时还是一级缓存的作用
+            Assert.assertEquals(role1.getRoleName(), role2.getRoleName());
+            Assert.assertSame(role1, role2);
+
+            role = role1;
+        }
+
+        // 当 sqlSession.close() 之后，SqlSession 才会保存查询数据到二级缓存中
+        System.out.println("开启新的 SqlSession");
+
+        try (SqlSession sqlSession = getSqlSession()) {
+            RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+
+            SysRole role1 = roleMapper.selectById(1L);
+            SysRole role2 = roleMapper.selectById(1L);
+            Assert.assertEquals("测试 L2Cache", role1.getRoleName());
+            Assert.assertSame(role1, role2);
+            Assert.assertSame(role, role1);
         }
     }
 }
