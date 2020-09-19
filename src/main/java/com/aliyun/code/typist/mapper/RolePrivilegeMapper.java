7aa46d9ce7f8dc03dc1bcee7de41aa2a2a536d26
Typist <z482728291@gmail.com>
2020-09-19 09:26:06 +0800

    使用 @SelectProvider 创建 SQL 的帮助类

--- src/main/java/com/aliyun/code/typist/mapper/RolePrivilegeMapper.java
+++ src/main/java/com/aliyun/code/typist/mapper/RolePrivilegeMapper.java
@@ -2,6 +2,7 @@ package com.aliyun.code.typist.mapper;
 
 import com.aliyun.code.typist.model.SysRolePrivilege;
 import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.jdbc.SQL;
 
 import java.util.List;
 
@@ -9,12 +10,23 @@ public interface RolePrivilegeMapper {
     @SelectProvider(type = RolePrivilegeProvider.class, method = "selectByRole")
     List<SysRolePrivilege> selectByRole(Long roleId);
 
+    @SelectProvider(type = RolePrivilegeProvider.class, method = "selectAll")
+    List<SysRolePrivilege> selectAll();
 
     // Provider 类的访问权限必须是 Public
     public static class RolePrivilegeProvider {
         // 方法必须返回 String 类型
         public String selectByRole(final Long roleId) {
             return "select * from sys_role_privilege where role_id = #{roleId}";
+        }
+
+        public String selectAll() {
+            return new SQL() {
+                {
+                    SELECT("role_id, privilege_id");
+                    FROM("sys_role_privilege");
+                }
+            }.toString();
         }
     }
 }
--- src/test/java/com/aliyun/code/typist/SysRolePrivilegeMapperTest.java
+++ src/test/java/com/aliyun/code/typist/SysRolePrivilegeMapperTest.java
@@ -3,8 +3,10 @@ package com.aliyun.code.typist;
 import com.aliyun.code.typist.mapper.RolePrivilegeMapper;
 import com.aliyun.code.typist.model.SysRolePrivilege;
 import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Collection;
 import java.util.List;
 
 public class SysRolePrivilegeMapperTest extends BaseMapperTest {
@@ -12,9 +14,30 @@ public class SysRolePrivilegeMapperTest extends BaseMapperTest {
     public void testSelectByRole() {
         try (SqlSession sqlSession = getSqlSession()) {
             List<SysRolePrivilege> rolePrivilegeList = sqlSession.getMapper(RolePrivilegeMapper.class).selectByRole(1L);
-            for(SysRolePrivilege rolePrivilege : rolePrivilegeList) {
-                System.out.println(rolePrivilege);
-            }
+
+            print(rolePrivilegeList);
+        }
+    }
+
+    @Test
+    public void testSelectAll() {
+        try(SqlSession sqlSession = getSqlSession()) {
+            List<SysRolePrivilege> rolePrivileges = sqlSession.getMapper(RolePrivilegeMapper.class).selectAll();
+
+            Assert.assertNotNull(rolePrivileges);
+            Assert.assertTrue(!rolePrivileges.isEmpty());
+
+            print(rolePrivileges);
+        }
+    }
+
+    private void print(Collection<SysRolePrivilege> rolePrivileges) {
+        if (null == rolePrivileges || rolePrivileges.isEmpty()) {
+            return;
+        }
+
+        for (SysRolePrivilege rolePrivilege : rolePrivileges) {
+            System.out.println(rolePrivilege);
         }
     }
 }
