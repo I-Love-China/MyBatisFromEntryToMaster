08704912c1cf70da28cfeeb2e6c515c5a721aa5f
Typist <z482728291@gmail.com>
2020-09-19 09:17:24 +0800

    @SelectProvider 1、Provider 类的访问权限需要是 Public 2、@SelectProvider method 指定的方法，其返回值需要是 String

--- src/main/java/com/aliyun/code/typist/mapper/RolePrivilegeMapper.java
+++ src/main/java/com/aliyun/code/typist/mapper/RolePrivilegeMapper.java
@@ -1,9 +1,22 @@
 package com.aliyun.code.typist.mapper;
 
 import com.aliyun.code.typist.model.SysRolePrivilege;
+import org.apache.ibatis.annotations.SelectProvider;
 
 import java.util.List;
 
 public interface RolePrivilegeMapper {
-    public List<SysRolePrivilege> selectByRole(Long roleId);
+    @SelectProvider(type = RolePrivilegeProvider.class, method = "selectByRole")
+    List<SysRolePrivilege> selectByRole(Long roleId);
+
+
+    // Provider 类的访问权限必须是 Public
+    public static class RolePrivilegeProvider {
+        // 方法必须返回 String 类型
+        public String selectByRole(final Long roleId) {
+            return "select * from sys_role_privilege where role_id = #{roleId}";
+        }
+    }
 }
+
+
